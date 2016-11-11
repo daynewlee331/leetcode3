d345780bada43765b599fb440e04cec5c12a30a8
Yi Li <yil@dhcp-wifi-8021x-155-41-12-212.bu.edu>
2016-11-10 19:44:53 -0500

    added low_high

--- higher_lower_guess.java
+++ higher_lower_guess.java
@@ -1,5 +1,25 @@
 package leetcode3;
 
 public class higher_lower_guess {
+	public int guessNumber(int n) {
+        if(n == 1) return 1;
+        long i = 1;
+        long j = n;
+        while(i <= j){
+            long mid = (i + j) / 2;
+            int flag = guess((int)mid);
+            if(flag == 0){
+                return (int)mid;
+            }else if(flag > 0){
+                i = mid + 1;
+            }else{
+                j = mid - 1;
+            }
+        }
+        return 0;
+    }
 	
+	public int guess(int mid){
+		return 0;
+	}
 }	
