e142ac75aafb615214dd877c6ed859b9597adbdc
Yi Li <yil@dhcp-wifi-8021x-155-41-78-32.bu.edu>
2016-11-14 17:06:43 -0500

    added valid+parenthese

---
+++ Valid_Parentheses.java
@@ -0,0 +1,33 @@
+package leetcode3;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+	public boolean isValid(String s) {
+        if(s == null || s.length() == 1) return false;
+        Stack<Character> stack = new Stack<Character>(); 
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '}'){
+                if(stack.size() == 0) return false;
+                char t = stack.pop();
+                if(t != '{') return false;
+            }else if(c == ']'){
+                if(stack.size() == 0) return false;
+                char t = stack.pop();
+                if(t != '[') return false;
+            }else if(c == ')'){
+                if(stack.size() == 0) return false;
+                char t = stack.pop();
+                if(t != '(') return false;
+            }else if(c == '('){
+                stack.push(c);
+            }else if(c == '['){
+                stack.push(c);
+            }else if(c == '{'){
+                stack.push(c);
+            }
+        }
+        return stack.size() == 0;
+    }
+}
