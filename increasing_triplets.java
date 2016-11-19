61d73c143d98412510abe95e7b4ab60f3cb0aa15
Yi Li <liyi64125837@163.com>
2016-11-18 17:28:11 -0700

    added increasing_triplets.java

---
+++ increasing_triplets.java
@@ -0,0 +1,15 @@
+package leetcode3;
+
+public class increasing_triplets {
+	public boolean increasingTriplet(int[] nums) {
+        int min = Integer.MAX_VALUE; int second_min = Integer.MAX_VALUE;
+        for(int num: nums){
+            if(num <= min){
+                min = num;
+            }else if(num < second_min){
+                second_min = num;
+            }else if(num > second_min) return true;
+        }
+        return false;
+    }
+}
