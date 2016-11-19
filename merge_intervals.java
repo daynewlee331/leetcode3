63a4e0226c4c002c627253dc432955dc43c2e08c
Yi Li <liyi64125837@163.com>
2016-11-19 16:48:01 -0700

    added my solution in merge_intervals

--- merge_intervals.java
+++ merge_intervals.java
@@ -1,5 +1,7 @@
 package leetcode3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,24 +15,41 @@ public class merge_intervals {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
+	
 	public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new Comparator<Interval>(){
-
+		if(intervals.size() < 1) return intervals;
+        Collections.sort(intervals,new Comparator<Interval>(){
 			@Override
 			public int compare(Interval o1, Interval o2) {
-				// TODO Auto-generated method stub
 				return o1.start - o2.start;
 			}
-        	
-        });
-        
-        PriorityQueue<Interval> queue = new PriorityQueue<Interval>(intervals.size(), new Comparator<Interval>(){
+			
+		});
+		
+		PriorityQueue<Interval> q = new PriorityQueue<Interval>(intervals.size(), new Comparator<Interval>(){
 			@Override
 			public int compare(Interval o1, Interval o2) {
 				// TODO Auto-generated method stub
-				return o1.end - o2.end;
+				return o2.end - o1.end;
 			}});
-        queue.offer(intervals.get(0));
-        
+		
+		// start with the first meeting, put it to a meeting room
+	    q.offer(intervals.get(0));
+		for(int i = 1; i < intervals.size(); i ++){
+			Interval current = q.poll();
+			if(intervals.get(i).start > current.end){//can't merge
+				q.offer(current);
+				q.offer(intervals.get(i));
+			}else{
+				Interval tmp = new Interval();
+				if(intervals.get(i).start < current.start) tmp.start = intervals.get(i).start;
+				else tmp.start = current.start;
+				if(intervals.get(i).end > current.end) tmp.end = intervals.get(i).end;
+				else tmp.end = current.end;
+				q.offer(tmp);
+			}
+		}
+		List<Interval> res = new ArrayList<Interval>(q);
+	    return res;
     }
 }
