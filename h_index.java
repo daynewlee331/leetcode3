a87cb6d682ca7ca47d255098ab0ac3bb8740da06
Yi Li <liyi64125837@163.com>
2017-01-02 10:53:21 +0800

    added h index 2

--- h_index.java
+++ h_index.java
@@ -3,6 +3,9 @@ package leetcode3;
 import java.util.Arrays;
 
 public class h_index {
+	//leecode: 274. H-Index
+	//A scientist has index h if h of his/her N papers have at least h citations each, 
+	//and the other N − h papers have no more than h citations each.
 	public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int h = 0;
@@ -11,5 +14,18 @@ public class h_index {
         	if(tmp > h) h = tmp;
         }
         return h;
+    }
+	
+	//leetcode 275. H-Index II
+	public int hIndexSorted(int[] citations) {
+		int len = citations.length;
+        int left = 0; int right = len - 1; 
+        while(left <= right){
+        	int mid = (left - right) / 2 + right;
+        	if(citations[mid] == len - mid) return citations[mid];
+        	else if(citations[mid] > (len - mid)) right = mid - 1;
+        	else left = mid + 1;
+        }
+        return len - right - 1;
     }
 }
