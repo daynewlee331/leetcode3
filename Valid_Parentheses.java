1f1f5b22e60f7e3c00cb366d6caa9be2ce598e4e
Yi Li <liyi64125837@163.com>
2017-01-04 17:25:43 +0800

    added leetcode 22. Generate Parentheses

--- Valid_Parentheses.java
+++ Valid_Parentheses.java
@@ -1,8 +1,47 @@
 package leetcode3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 /* *** *** */
 public class Valid_Parentheses {
+	public static void main(String[] args){
+		Valid_Parentheses vp = new Valid_Parentheses();
+		List<String> res = vp.generateParenthesis(4);
+		for(String s: res){
+			System.out.print(s + " ");
+		}
+	}
+	
+	//leetcode 22. Generate Parentheses
+	public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        generateParenthesisHelper(n, n, new StringBuilder(), res);
+        return res;
+    }
+	
+	public void generateParenthesisHelper(int left, int right
+			, StringBuilder cur, List<String> res){
+		if(left > right) return;
+		if(left == 0 && right == 0){
+			String s = cur.toString();
+			res.add(s);
+			return;
+		}
+		
+		if(left - 1 >= 0){
+			cur.append('(');
+			generateParenthesisHelper(left - 1, right, cur, res);
+			cur.setLength(cur.length() - 1);
+		}
+		if(right - 1 >= 0){
+			cur.append(')');
+			generateParenthesisHelper(left, right - 1, cur, res);
+			cur.setLength(cur.length() - 1);
+		}
+	}
+	
+	//leetcode 20. Valid Parentheses
 	public boolean isValid(String s) {
         if(s == null || s.length() < 1) return false;
         Stack<Character> stack = new Stack<Character>(); 
