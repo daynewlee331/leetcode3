f7a8ff633b3f15dd714ab280873d634df5880fcd
Yi Li <liyi64125837@163.com>
2017-01-10 17:18:23 +0800

    added leetcode 81. Search in Rotated Sorted Array II

--- search_rotated_array.java
+++ search_rotated_array.java
@@ -6,6 +6,35 @@ public class search_rotated_array {
 		search_rotated_array sr = new search_rotated_array();
 		sr.search(test, 8);
 	}
+	
+	//leetcode 81. Search in Rotated Sorted Array II
+	public boolean search2(int[] nums, int target) {
+		if(nums == null || nums.length < 1) return false;
+        int i = 0;
+        int j = nums.length - 1;
+        while(i <= j){
+            int mid = (i + j) / 2;
+            if(nums[mid] == target) return true;
+            if(nums[mid] < nums[i]){//mid is on the right
+            	//only consider the most right part because that part is in order
+            	if(target > nums[mid] && target <= nums[j]){
+            		i = mid + 1;
+            	}else{
+            		j = mid - 1;
+            	}
+            }else if(nums[mid] > nums[i]){//mid is on the left
+            	//only consider the most left part because it is in order
+            	if(target >= nums[i] && target < nums[mid]){
+            		j = mid - 1;
+            	}else{
+            		i = mid + 1;
+            	}
+            }else i ++;
+        }
+        
+        return false;
+    }
+	
 	//leetcode 153. Find Minimum in Rotated Sorted Array
 	public int findMin(int[] nums) {
         int left = 0;
