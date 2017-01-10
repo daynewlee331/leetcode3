bbdf38e3603bc661f0d0a0c68cb407517bc76814
Yi Li <liyi64125837@163.com>
2017-01-11 00:16:53 +0800

    added search rotated array with duplicates

--- search_rotated_array.java
+++ search_rotated_array.java
@@ -8,6 +8,13 @@ public class search_rotated_array {
 	}
 	
 	//leetcode 81. Search in Rotated Sorted Array II
+	/*
+	 * To explain why, consider this sorted array 1111115, which is rotated to 1151111.
+	 * Assume left = 0 and mid = 3, and the target we want to search for is 5. 
+	 * Therefore, the condition A[left] == A[mid] holds true, which leaves us with only two possibilities:
+	 * All numbers between A[left] and A[right] are all 1's.
+	 * Different numbers (including our target) may exist between A[left] and A[right].
+	 * */
 	public boolean search2(int[] nums, int target) {
 		if(nums == null || nums.length < 1) return false;
         int i = 0;
