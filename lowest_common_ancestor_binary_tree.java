22987f0a74c5de433a8b8add07a20cf7b6db6a31
Yi Li <liyi64125837@163.com>
2017-02-01 08:49:44 -0500

    added LC 235

--- lowest_common_ancestor_binary_tree.java
+++ lowest_common_ancestor_binary_tree.java
@@ -10,4 +10,19 @@ public class lowest_common_ancestor_binary_tree {
         //else if(left != null && right == null) return left;
         //else return right;
     }
+	
+	//input: might be nulls
+	//if p and q are both bigger than root, the LCA is in the right subtree
+	//if p and q are both smaller than root, the LCA is in the left subtree
+	//else root the LCA
+	public TreeNode lowestCommonAncestorBST(TreeNode root, TreeNode p, TreeNode q) {
+		if(root == null || p == null || q == null) return null;
+		return helper(root, p, q);
+	}
+	
+	public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
+		if(root.val < p.val && root.val < q.val) return helper(root.right, p, q);
+		else if(root.val > p.val && root.val > q.val) return helper(root.left, p, q);
+		else return root;
+	}
 }
