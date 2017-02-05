8dfe2161f4ecc5709ff5e865aa8fb41e04aedb24
Yi Li <liyi64125837@163.com>
2017-02-05 14:08:14 -0500

    added new solution for LC79

--- word_search.java
+++ word_search.java
@@ -1,7 +1,5 @@
 package leetcode3;
 
-import java.util.HashSet;
-
 public class word_search {
 	public static void main(String[] args) {
 		word_search ws = new word_search();
@@ -10,33 +8,26 @@ public class word_search {
 	}
 
 	public boolean exist(char[][] board, String word) {
-		if (board == null || board.length < 1 || board[0].length < 1)
-			return false;
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				boolean[][] visited = new boolean[board.length][board[0].length];
-				if (search(board, word, i, j, 0, visited))
-					return true;
+		if(word == null || word.length() < 1 || board.length < 1) return false;
+		for(int i = 0; i < board.length; i ++){
+			for(int j = 0; j < board[0].length; j ++){
+				if(findWord(word, i, j, 0, board)) return true;
 			}
 		}
 		return false;
 	}
-
-	public boolean search(char[][] board, String word, int row, int col, int index, boolean[][] visited) {
-		if (index == word.length())
-			return true;
-		if (col >= board[0].length || row >= board.length || row < 0 || col < 0 || visited[row][col])
-			return false;
-		if (board[row][col] != word.charAt(index))
-			return false;
-
-		visited[row][col] = true;
-		boolean res = search(board, word, row, col + 1, index + 1, visited)
-				|| search(board, word, row + 1, col, index + 1, visited)
-				|| search(board, word, row - 1, col, index + 1, visited)
-				|| search(board, word, row, col - 1, index + 1, visited);
-		visited[row][col] = false;
-
+	
+	public boolean findWord(String word, int row, int col, int index, char[][] board){
+		if(word.length() == index) return true;
+		if(row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+		if(word.charAt(index) != board[row][col]) return false;
+		char c = board[row][col];
+		board[row][col] = '*';
+		boolean res = findWord(word, row + 1, col, index + 1, board) ||
+						findWord(word, row - 1, col, index + 1, board) ||
+						findWord(word, row, col + 1, index + 1, board) ||
+						findWord(word, row, col - 1, index + 1, board);
+		board[row][col] = c;
 		return res;
 	}
 
