96304854279381e6e34757547ce366f3e66e3c73
Yi Li <liyi64125837@163.com>
2017-02-12 11:32:14 -0500

    added new solution for round_sum.java

--- round_sum.java
+++ round_sum.java
@@ -2,6 +2,7 @@ package leetcode3;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class round_sum {
 	public static void main(String[] args){
@@ -35,6 +36,35 @@ public class round_sum {
 	 * input = 30.9, 2.4, 3.9
 	 * output = 31 2 4
 	 * */
+	
+	public int[] roundNumsTwo(double[] a) {
+		if (a == null || a.length < 1) return new int[0];
+		int[] res = new int[a.length];
+		double originSum = 0;
+		long floorSum = 0;
+		PriorityQueue<RoundedNum> pq = new PriorityQueue<>(new Comparator<RoundedNum>(){
+			@Override
+			public int compare(RoundedNum o1, RoundedNum o2) {
+				// TODO Auto-generated method stub
+				if(o2.val > o1.val) return 1;
+				else if(o2.val < o1.val) return -1;
+				else return 0;
+			}
+		});
+		for (int i = 0; i < a.length; i++) {
+			floorSum += (int) a[i];
+			originSum += a[i];
+			res[i] = (int) a[i];
+			pq.offer(new RoundedNum(i, a[i] - (int)a[i]));
+		}
+		double remaining = (int)(Math.round(originSum) - floorSum);
+		for(int i = 0; i < remaining; i ++){
+			RoundedNum tmp = pq.poll();
+			res[tmp.index] ++;
+		}
+		return res;
+	}
+	
 	public int[] getRounded(double[] nums){
 		int[] res = new int[nums.length];
 		RoundedNum[] list = new RoundedNum[nums.length];
