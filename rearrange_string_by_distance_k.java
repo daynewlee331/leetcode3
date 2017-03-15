a197f12a5397cb9c0459deae15b65c1c46ce0ed6
Yi Li <liyi64125837@163.com>
2017-03-14 22:09:56 -0400

    added new solution for LC358

--- rearrange_string_by_distance_k.java
+++ rearrange_string_by_distance_k.java
@@ -7,54 +7,47 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class rearrange_string_by_distance_k {
-	public static void main(String[] args){
-		rearrange_string_by_distance_k sol = new rearrange_string_by_distance_k();
-		String test = "aaabc";
-		String res = sol.rearrangeString(test, 2);
-		System.out.println(res);
-	}
-	
-	public String rearrangeString(String s, int k) {
-        if(s == null || s.length() < 1 || k < 1) return "";
-        HashMap<Character, Node> map = new HashMap<>();
-        for(int i = 0; i < s.length(); i ++){
-        		char c = s.charAt(i);
-        		if(!map.containsKey(c)) map.put(c, new Node(c, 0));
-        		map.get(c).count ++;
-        }
-        
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>(){
-			@Override
-			public int compare(Node o1, Node o2) {
-				// TODO Auto-generated method stub
-				if(o2.count == o1.count) return o1.ch - o2.ch;
-				else return o2.count - o1.count;
-			}});
-        
-        pq.addAll(map.values());
-        StringBuilder builder = new StringBuilder();
-        List<Node> buffer = new ArrayList<Node>();
-        while(!pq.isEmpty()){
-        		buffer.clear();
-        		for(int i = 0; i < k; i ++){
-        			if(builder.length() == s.length()) return builder.toString();
-        			if(pq.isEmpty()) return "";
-        			Node e = pq.poll();
-        			e.count --;
-        			builder.append(e.ch);
-        			if(e.count > 0)	buffer.add(e);
-        		}
-        		pq.addAll(buffer);
-        }
-        return builder.toString();
+	class Element{
+	    int fre;
+	    char ch;
+	    public Element(char c, int x){
+		    this.fre = x;
+		    this.ch = c;
+	    }
     }
-	
-	class Node{
-		int count = 0;
-		char ch = '*';
-		public Node(char c, int x){
-			this.ch = c;
-			this.count = x;
+    
+public String rearrangeString(String s, int k) {
+    if(s == null || s.length() < 1 || k < 0) return "";
+    if(k == 0) return s;
+    PriorityQueue<Element> pq = new PriorityQueue<Element>(new Comparator<Element>(){
+		@Override
+		public int compare(Element o1, Element o2) {
+			// TODO Auto-generated method stub
+			if(o2.fre != o1.fre) return o2.fre - o1.fre;
+			else return o1.ch - o2.ch;
 		}
-	}
+    });
+    HashMap<Character, Element> map = new HashMap<Character, Element>();
+    for(int i = 0; i < s.length(); i ++){
+    		char c = s.charAt(i);
+    		if(!map.containsKey(c)) map.put(c, new Element(c, 0));
+    		map.get(c).fre ++;
+    }
+    pq.addAll(map.values());
+    List<Element> buff = null;
+    StringBuilder builder = new StringBuilder();
+    while(!pq.isEmpty()){
+    		buff = new ArrayList<Element>();
+    		for(int i = 0; i < k; i ++){
+    			if(builder.length() == s.length()) return builder.toString();
+    			if(pq.isEmpty()) return "";
+    			Element e = pq.poll();
+    			builder.append(e.ch);
+    			e.fre --;
+    			if(e.fre > 0) buff.add(e);
+    		}
+    		pq.addAll(buff);
+    }
+    return builder.toString();
+}
 }
